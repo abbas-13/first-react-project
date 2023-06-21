5d4a911296057b7ec551463c5dc6c91c3faa79d2
mohammad abbas <abbasathe61@astronaut-2.local>
2023-06-21 15:58:16 +0530

    remove unrequired code

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 export const Sidebar = () => {
   const links = [
+    { label: "Login", path: "/Login" },
     { label: "Home", path: "/" },
     { label: "About Us", path: "/AboutUs" },
     { label: "Contact Us", path: "/ContactUs" },
@@ -11,10 +12,10 @@ export const Sidebar = () => {
   ];
 
   const renderedLinks = links.map((link) => (
-      <li key={link.label} className="mb-2">
-        <Link to={link.path}>{link.label}</Link>
-      </li>
-    ));
+    <li key={link.label} className="mb-2">
+      <Link to={link.path}>{link.label}</Link>
+    </li>
+  ));
 
   return (
     <div className="w-48 h-full bg-slate-200 border-r-2 border-gray-300 p-4">
