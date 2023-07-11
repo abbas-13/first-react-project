9b11cea4527aa331dc8ad83e90044603d5f5ddd0
mohammad abbas <abbasathe61@astronaut-2.local>
2023-07-12 01:45:56 +0530

    Add authentication functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 export const Navbar = () => (
-    <nav className="bg-slate-200 border-gray-300 border-b-2 py-4">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-6">
-          <div className="hidden md:block"></div>
-        </div>
+  <nav className="bg-slate-200 border-gray-300 border-b-2 py-4">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex justify-between h-6">
+        <div className="hidden md:block"></div>
       </div>
-    </nav>
-  );
+    </div>
+  </nav>
+);
